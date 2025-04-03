7c808342756a5c206c8ed98c735c28e03745ede5
Henry Dickman <cardsnapper42@gmail.com>
2025-04-03 02:58:53 +0200

    MAIN MENU + BADASS DEVICE SELECT
    -Basic main menu finished
    -Device select finished
    
    Too tired to implement a pause menu for reinputting devices during game. I've done my task for the day. Also there is one new bug where Siepie can't see anything on her side of the screen but y'know I didn't make the camera so that's not my problem. Btw this is probably the shitties implementation of all time BUT IT'S DONE.

--- Assets/Scripts/Player/ControllerMover.cs
+++ Assets/Scripts/Player/ControllerMover.cs
@@ -3,18 +3,32 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class ControllerMover : MonoBehaviour
 {
-    public Transform[] Targets;
-    public static Action<GameObject, int> controlCheck;
+    TextMeshProUGUI screenText;
+    InputDevice device;
+    Transform[] Targets = new Transform[3];
+    Transform BG;
+    public static Action<GameObject, int, InputDevice> controlCheck;
     bool locked = false;
     float moveValue;
     int location = 1;
 
     private void Start()
     {
-        MainMenuManager.returnCheck += Checked;
+        BG = GameObject.Find("Sky").transform;
+        transform.SetParent(BG);
+        device = GetComponent<PlayerInput>().devices[0];
+        screenText = GetComponent<TextMeshProUGUI>();
+        gameObject.name = device.name;
+        screenText.text = device.name;
+        TransMenuManager.returnCheck += Checked;
+        Targets[0] = GameObject.Find("Left").transform;
+        Targets[1] = GameObject.Find("Middle").transform;
+        Targets[2] = GameObject.Find("Right").transform;
+        transform.position = Targets[1].position;
     }
 
     public void OnMove(InputValue value)
@@ -26,10 +40,11 @@ public class ControllerMover : MonoBehaviour
     {
         if (!locked)
         {
-            controlCheck?.Invoke(this.gameObject, location);
+            controlCheck?.Invoke(this.gameObject, location, device);
         }
         else
         {
+            screenText.color = Color.white;
             locked = false;
         }
     }
@@ -38,6 +53,7 @@ public class ControllerMover : MonoBehaviour
     {
       if(this.gameObject == controllerChecked)
       {
+            screenText.color = Color.white;
             locked = true;
       }  
     }
@@ -49,6 +65,10 @@ public class ControllerMover : MonoBehaviour
             if (moveValue < 0f && location != 0) location--;
             
             else if (moveValue > 0f && location != 2)location++;
+        }
+        else
+        {
+            screenText.color = Color.green;
         }
         transform.position = Vector3.Lerp(transform.position, Targets[location].position, 0.5f);
     }
