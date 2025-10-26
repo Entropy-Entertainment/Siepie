5960669461d1b6c7fbcfc7b722db5a540996167b
Nxbi <37214@ma-web.nl>
2025-10-26 13:00:38 +0100

    Finished writing the PlayerInteractor_Range test

--- Assets/Testing/Runtime/Player/PlayerInteractor_Range.cs
+++ Assets/Testing/Runtime/Player/PlayerInteractor_Range.cs
@@ -1,10 +1,11 @@
-using System.Collections;
 using NUnit.Framework;
+using Player.Interaction;
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
-using Player.Interaction;
 
 public class PlayerInteractor_Range : InputTestFixture
 {
@@ -12,41 +13,63 @@ public class PlayerInteractor_Range : InputTestFixture
   GameObject stubPlayer;
   GameObject stubNpc;
   PlayerInteractor playerInteractor;
-  [SetUp]
-  public void TestSetup()
+  Action<GameObject, GameObject> interactionHandler;
+  bool playerInteractFired = false;
+
+  [UnitySetUp]
+  public IEnumerator TestSetup()
   {
     // Load the test scene
     SceneManager.LoadScene("GeneralTestScene");
+    // Wait a frame so the scene and its GameObjects are available
+    yield return null;
+
     // Create stub input device
     stubController = InputSystem.AddDevice<Gamepad>();
-    // Create a stub player object with PlayerInteractor component
+
+    // In the test scene, there is a Player object already
     stubPlayer = GameObject.Find("Player");
+    Assert.IsNotNull(stubPlayer, "Player GameObject not found in GeneralTestScene");
+
     if (!stubPlayer.TryGetComponent<PlayerInteractor>(out playerInteractor))
-      stubPlayer.AddComponent<PlayerInteractor>();
+      playerInteractor = stubPlayer.AddComponent<PlayerInteractor>();
+
     // Create dummy NPC to interact with
     stubNpc = new GameObject("StubNpc");
-
     stubNpc.AddComponent<NpcInteract>();
+
+    // Subscribe to PlayerInteract event to see that when it fires 
+    interactionHandler = (player, interactedObject) => playerInteractFired = true;
+    PlayerInteractor.PlayerInteract += interactionHandler;
 
     // Set locations outside of interaction range
     stubPlayer.transform.position = Vector3.zero;
-    stubNpc.transform.position = new Vector3(playerInteractor.InteractDistance + 1, 0);
+    stubNpc.transform.position = new Vector2(playerInteractor.InteractDistance, 0) + Vector2.right;
 
-    // Create stub class that can check if OnInteract was called
+    yield break;
   }
+
   [UnityTest]
-  public IEnumerator PlayerInteractWithEnumeratorPasses()
+  public IEnumerator PlayerInteractor_Range_Mono()
   {
     Set(stubController.leftStick, Vector2.right);
     // The button on gamepads that maps to player interaction
     Set(stubController.buttonWest, 1.0f);
     InputSystem.Update();
-    // If npc is out of range, conversation should not start
-    if (Vector3.Distance(stubPlayer.transform.position, stubNpc.transform.position) > playerInteractor.InteractDistance)
-     // Assert.That(NpcInteract.InConversation == false);
 
+    if (playerInteractFired == true)
+    {
+      Assert.That(Vector3.Distance(stubPlayer.transform.position, stubNpc.transform.position) <= playerInteractor.InteractDistance);
+    }
+    else yield return null;
+  }
 
-
-    yield return null;
+  [TearDown]
+  public void Teardown()
+  {
+    UnityEngine.Object.Destroy(stubNpc);
+    PlayerInteractor.PlayerInteract -= interactionHandler;
+    InputSystem.RemoveDevice(stubController);
+    SceneManager.UnloadSceneAsync("GeneralTestScene");
   }
 }
