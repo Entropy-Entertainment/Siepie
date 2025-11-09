a1e94e723ad8d59b1a7b7822abfa864f664273ba
Nxbi <noradegreeuw@gmail.com>
2025-11-09 20:14:52 +0100

    Added XML for DialogDeserializer

--- Assets/Scripts/Dialog/DialogDeserializer.cs
+++ Assets/Scripts/Dialog/DialogDeserializer.cs
@@ -1,6 +1,12 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// Deserializes dialog data from JSON files located in the Resources/DialogData/ directory.
+/// LoadResoucesAPI() must be called for the first scene's loading.
+/// Listens for scene changes to load the appropriate dialog data for the new scene.
+/// Should work completely automatically after initialization in InitDialogSystem.
+/// </summary>
 public class DialogDeserializer : Deserializer<DialogData>
 {
   public string DialogFilePath { get => dialogFilePath; private set => dialogFilePath = value; }
@@ -22,7 +28,5 @@ public class DialogDeserializer : Deserializer<DialogData>
     {
       GetDeserializedObject();
     }
-    
-
   }
 }
