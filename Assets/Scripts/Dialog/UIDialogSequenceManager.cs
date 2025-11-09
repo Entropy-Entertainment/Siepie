584cfcd71eb4bc33fd917af77815d2cf78f1047f
Nxbi <noradegreeuw@gmail.com>
2025-11-09 20:13:27 +0100

    Started on the code for the UI

---
+++ Assets/Scripts/Dialog/UIDialogSequenceManager.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+/// <summary>
+/// The class that manages the UI prefab DialogTemplate.
+/// This class must link to an INpcDialog object to receive dialog updates.
+/// </summary>
+public class UIDialogSequenceManager : MonoBehaviour
+{ 
+  [SerializeField] UIDocument UI;
+  Label currentSpeaker;
+  Label dialogTextDisplay;
+  Label speakerLeft;
+  Label speakerRight;
+  INpcDialog npcDialogProvider;
+
+  void Start()
+  {
+    currentSpeaker = UI.rootVisualElement.Q<Label>("CurrentlySpeaking");
+    dialogTextDisplay = UI.rootVisualElement.Q<Label>("Text");
+    speakerLeft = UI.rootVisualElement.Q<Label>("SpeakerLeft");
+    speakerRight = UI.rootVisualElement.Q<Label>("SpeakerRight");
+  }
+
+  /// <summary>
+  /// Make sure to invoke UpdateDialog event from the INpcDialog object after calling this method to set the initial dialog values.
+  /// </summary>
+  /// <param name="npcDialogObj">The npc from which this class will recieve updates</param>
+  public void StartUIDialogSequence(INpcDialog npcDialogObj)
+  {
+    npcDialogObj.UpdateDialog += SetDialogFirstTime;
+
+    void SetDialogFirstTime(string speakerLeftName, string speakerRightName, string initialText)
+    {
+      speakerLeft.text = speakerLeftName;
+      speakerRight.text = speakerRightName;
+      dialogTextDisplay.text = initialText;
+      dialogTextDisplay.style.display = DisplayStyle.None;
+      npcDialogObj.UpdateDialog -= SetDialogFirstTime;
+    }
+    LinkUpdateUIEvent(npcDialogObj);
+  }
+
+  /// <summary>
+  /// UIDialogSequenceManager links to an INpcDialog object to receive dialog updates.
+  /// Only one INpcDialog object can be linked at a time.
+  /// </summary>
+  /// <param name="npcDialogObj">The npc from which this class will recieve updates</param>
+  internal void LinkUpdateUIEvent(INpcDialog npcDialogObj)
+  {
+    UnlinkUpdateUIEvent();
+    npcDialogProvider = npcDialogObj;
+    npcDialogProvider.UpdateDialog += updateUI;
+  }
+
+  /// <summary>
+  /// Updated the UI dialog box with the provided parameters.
+  /// </summary>
+  /// <param name="speakerName">The character on the left of the dialog box</param>
+  /// <param name="listener">The npc with whom the player interacted with (unless the parameters get modified in the NpcInteract class) </param>
+  /// <param name="dialogText">The dialog to be displayed in the uxml UI</param>
+  void updateUI(string speakerName, string listener, string dialogText)
+  {
+    if (currentSpeaker != null)
+      currentSpeaker.text = speakerName;
+
+    if (dialogTextDisplay != null)
+      dialogTextDisplay.text = dialogText;
+  }
+
+  void UnlinkUpdateUIEvent()
+  {
+    if (npcDialogProvider != null)
+    {
+      npcDialogProvider.UpdateDialog -= updateUI;
+      npcDialogProvider = null;
+    }
+  }
+}
--- Assets/Scripts/InteractableObjs/NpcInteract.cs
+++ Assets/Scripts/InteractableObjs/NpcInteract.cs
@@ -1,23 +1,34 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
 namespace Player.Interaction
 {
-  public class NpcInteract : MonoBehaviour, IInteractable
+  public class NpcInteract : MonoBehaviour, IInteractable, INpcDialog
   {
+    public event Action<string, string, string> UpdateDialog;
     [SerializeField] internal int[] dialogUID;
+    static internal Deserializer<DialogData> dialogDeserializer;
     DialogData.DialogLine[] dialogLines;
+    [SerializeField] GameObject UIDialogPrefab;
+    UIDialogSequenceManager UIDialogSequenceManager;
     int currentSequence;
-    static internal Deserializer<DialogData> dialogDeserializer = new("DialogData");
+   
     void Start()
     {
+      dialogDeserializer = InitDialogSystem.dialogDeserializer;
       SubscribeToInteractEvent();
     }
 
     public void PlayerInteracted(GameObject player, GameObject interactedObject)
     {
-      if (interactedObject == this.gameObject) currentSequence++; 
+      if (interactedObject == this.gameObject)
+      {
+        GetAllRelevantUID();
+        UIDialogSequenceManager = Instantiate(UIDialogPrefab).GetComponent<UIDialogSequenceManager>();
+        UIDialogSequenceManager.LinkUpdateUIEvent(this);
+      }
     }
 
     internal void GetAllRelevantUID()
