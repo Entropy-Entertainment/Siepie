179263ab04145d3364ee4dfe133b180e35442cb3
Nxbi <noradegreeuw@gmail.com>
2025-11-11 13:51:18 +0100

    Made NpcInteract SRP again

--- Assets/Scripts/InteractableObjs/NpcInteract.cs
+++ Assets/Scripts/InteractableObjs/NpcInteract.cs
@@ -1,46 +1,32 @@
 using System;
-using System.Linq;
 using UnityEngine;
 
 namespace Player.Interaction
 {
-  public class NpcInteract : MonoBehaviour, IInteractable, INpcDialog
+  public class NpcInteract : MonoBehaviour, IInteractable
   {
-    public event Action<string, string, string> UpdateDialog;
-    [SerializeField] internal int[] dialogUID;
-    static internal Deserializer<DialogData> dialogDeserializer;
-    DialogData.DialogLine[] dialogLines;
-    [SerializeField] GameObject UIDialogPrefab;
-    UIDialogSequenceManager UIDialogSequenceManager;
-    int currentSequence;
-   
+    public Action<GameObject, GameObject> WasInteractedWith;
+    bool firstTime = true;
+
     void Start()
     {
-      dialogDeserializer = InitDialogSystem.dialogDeserializer;
       SubscribeToInteractEvent();
     }
 
     public void PlayerInteracted(GameObject player, GameObject interactedObject)
     {
       if (interactedObject == this.gameObject)
-      {
-        GetAllRelevantUID();
-        UIDialogSequenceManager = Instantiate(UIDialogPrefab).GetComponent<UIDialogSequenceManager>();
-        UIDialogSequenceManager.LinkUpdateUIEvent(this);
+      { 
+        WasInteractedWith.Invoke(player, this.gameObject);
       }
     }
-
-    internal void GetAllRelevantUID()
-    {
-      dialogLines = dialogDeserializer.GetDeserializedObject().Lines
-        .Where(aDialogLine => dialogUID.Contains(aDialogLine.UID))
-        .ToArray();
-      print(dialogLines.Length);
-    }
-
     public void SubscribeToInteractEvent()
     {
-      PlayerInteractor.PlayerInteract += PlayerInteracted;
+      if (firstTime)
+      {
+        PlayerInteractor.PlayerInteract += PlayerInteracted;
+        firstTime = false;
+      }
     }
 
     void OnDestroy()
